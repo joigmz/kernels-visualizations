87a972d9cc15a83e7a33eaf7c45b73977834b543
jose-izam99 <57118197+jose-izam99@users.noreply.github.com>
2021-12-24 18:29:54 -0300

    MVP

--- app.py
+++ app.py
@@ -11,43 +11,56 @@ server = app.server
 
 app.layout = html.Div([
     html.Div(
-        html.Div(dcc.Dropdown(
-            id="Dropdown-symbols",
-            options=[
-            {'label': i, 'value': i} for i in range(1,10,1)
-            ],
-            value=1),
-            style={'width': '50%'}),
-        style={ 'display': 'flex', 'justify-content': 'center'}),
+        dash.html.H1("Squared exponential kernel"),
+    style={ 'display': 'flex', 'justify-content': 'center'}),
+
+    html.Div(
+        html.Div(
+            dcc.Slider(
+                id='slider-updatemode',
+                marks={i: '{}'.format(10 ** i) for i in range(4)},
+                max=3,
+                value=2,
+                step=0.01,
+                updatemode='drag'
+            ),
+        style={'width': '50%'}),
+    style={ 'display': 'flex', 'justify-content': 'center'}),
+
+    html.Div(
+        html.Div(
+            id='updatemode-output-container', 
+        style={'margin-top': 5}),
+    style={ 'display': 'flex', 'margin': '0px 25% 0px 25%'}),
+
     html.Br(),
     html.Div(
         html.Div(dcc.Graph(id='my-output'),
         style={'width': '70%'}),
     style={ 'display': 'flex', 'justify-content': 'center'}),
+    html.Hr(),
 ])
+
 @app.callback(
     Output('my-output','figure'),
-    Input('Dropdown-symbols', 'value'))
-
+    Input('slider-updatemode', 'value'))
 def update_output_div(Input):
 
     X_k = np.arange(-4,4,0.5).reshape(-1, 1)
     l = Input
     K1 = Squared_exponential_kernel(X_k, X_k, l)
 
-    xs,ys = [],[]
-    zs1 = []
-
-    for i in range(K1.shape[0]):
-        for j in range(K1.shape[1]):
-            xs.append(X_k[i][0])
-            ys.append(X_k[j][0])
-            zs1.append(K1[i,j])
-
-    df = pd.DataFrame(np.array([xs,ys,zs1]).T, columns = ['x','y','z'])
+    df = data_3dplot(X_k,K1)
 
     fig = px.scatter_3d(df, x='x', y='y', z='z')
     return fig
+
+@app.callback(
+    Output('updatemode-output-container', 'children'),
+    Input('slider-updatemode', 'value'))
+def update_output(value):
+    return 'l: {}'.format(value)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- kernels.py
+++ kernels.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics.pairwise import euclidean_distances
 
 def Squared_exponential_kernel(X, Y, l):
@@ -29,3 +30,16 @@ def Polynomial_kernel(X, Y, sigmab, sigmav, mu, alpha):
     kernel = sigmab**2 + (sigmav**2)*(X-mu)@(Y-mu).T
     kernel = kernel**alpha
     return kernel
+
+def data_3dplot(X_k,K1):
+    xs,ys = [],[]
+    zs1 = []
+
+    for i in range(K1.shape[0]):
+        for j in range(K1.shape[1]):
+            xs.append(X_k[i][0])
+            ys.append(X_k[j][0])
+            zs1.append(K1[i,j])
+
+    df = pd.DataFrame(np.array([xs,ys,zs1]).T, columns = ['x','y','z'])
+    return df
